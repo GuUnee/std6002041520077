4a65082a5c12577e426ff7fccf22171be44ad663
ไพรัช จันทร์ไข่ <s6002041520077@Macmini-213-30.local>
2019-03-21 12:46:02 +0700

    first commit

---
+++ components/app.js
@@ -0,0 +1,17 @@
+//import library
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+//component
+class App extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text> hello pairat 
+                </Text>
+            </View>
+        );
+    }
+}
+export default App;
