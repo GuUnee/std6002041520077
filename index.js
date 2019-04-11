657bf0a6b84ed469dff1ad3b63386f08b579fe3b
ไพรัช จันทร์ไข่ <s6002041520077@Macmini-213-30.local>
2019-04-11 15:34:41 +0700

    chang screen

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 /**
  * @format
  */
-
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 import {AppRegistry} from 'react-native';
 //import App from './App';
 import {name as appName} from './app.json';
 import App from './components/App';
 import About from './components/About';
+import Login from './components/Login';
 
-
+const MainNavigator = createStackNavigator({
+    Login: {screen: Login},
+    About: {screen: About},
+  });
+const App = createAppContainer(MainNavigator);
 AppRegistry.registerComponent(appName, () => App);
 
